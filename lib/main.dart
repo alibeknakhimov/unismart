afd3c21e06c3bb4d21487bc852f8e0597ffcfe09
alibeknakhimov <mega.nakhimovs5@gmail.com>
2024-02-15 20:11:06 +0600

    f

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:unismart/unicontrol.dart';
 
-// Model representing the data structure
-
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure that Flutter bindings are initialized
-  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
-  await Firebase.initializeApp(); // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
--- lib/unicontrol.dart
+++ lib/unicontrol.dart
@@ -13,115 +13,179 @@ class User {
   }
 }
 
-class UserList extends StatelessWidget {
-  final double leftPadding;
-  final double rightPadding;
-  final double topPadding;
-  final double bottomPadding;
+class UserList extends StatefulWidget {
+  @override
+  _UserListState createState() => _UserListState();
+}
 
-  UserList({
-    this.leftPadding = 4.0,
-    this.rightPadding = 4.0,
-    this.topPadding = 210.0,
-    this.bottomPadding = 0.0,
-  });
+class _UserListState extends State<UserList> {
+  int? selectedCab;
 
   @override
   Widget build(BuildContext context) {
-    double displayWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Color.fromARGB(255, 29, 99, 20),
-              Color.fromARGB(126, 2, 145, 6),
-            ],
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(
-            leftPadding,
-            topPadding,
-            rightPadding,
-            bottomPadding,
-          ),
-          child: StreamBuilder(
-            stream: FirebaseFirestore.instance
-                .collection('users')
-                .doc('rasul')
-                .snapshots(),
-            builder: (BuildContext context,
-                AsyncSnapshot<DocumentSnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc('rasul')
+          .snapshots(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
 
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
 
-              User user = User.fromFirestore(snapshot.data!);
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "Hello, Alibek",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.w800),
-                  ),
-                  SizedBox(height: 80), // Add space between image and blocks
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(35),
-                        color: Color.fromARGB(45, 2, 145, 6),
+        User user = User.fromFirestore(snapshot.data!);
+        return Container(
+          padding: EdgeInsets.only(top: 100),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 10),
+                child: Row(
+                  children: [
+                    DropdownButton<int>(
+                      hint: Text(
+                        'Выберите кабинет ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black,
+                            fontSize: 26),
+                      ), // Displayed when no value is selected
+                      value: selectedCab,
+                      underline: Container(),
+                      icon: Icon(
+                        Icons.arrow_drop_down_circle,
+                        color: Colors.black,
                       ),
-                      padding: EdgeInsets.only(
-                          left: 15, right: 15, top: 15, bottom: 15),
-                      child: GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 24.0,
-                          crossAxisSpacing: 16.0,
-                          childAspectRatio: 1.34,
-                        ),
-                        itemCount: user.cabs.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: const Color.fromARGB(255, 255, 255, 255),
-                            ),
-                            child: Center(
-                              child: Text(
-                                '${user.cabs[index]}',
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 0, 0, 0),
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
+                      items: user.cabs.map<DropdownMenuItem<int>>((int value) {
+                        return DropdownMenuItem<int>(
+                          value: value,
+                          child: Text(
+                            'Кабинет $value',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                color: Colors.black,
+                                fontSize: 27),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (int? newValue) {
+                        setState(() {
+                          selectedCab = newValue;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Expanded(
+                child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Color.fromARGB(255, 159, 206, 95),
+                          Color.fromARGB(255, 14, 128, 40),
+                        ],
+                      ),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(50),
                       ),
                     ),
-                  ),
+                    padding: EdgeInsets.only(
+                      top: 45,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(child: Container()),
+                            ElevatedButton(
+                              onPressed: () => {},
+                              child: Column(
+                                // Replace with a Row for horizontal icon + text
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.sunny,
+                                    size: 50,
+                                  ),
+                                  Text("Свет")
+                                ],
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0.0),
+                            ),
+                            Expanded(child: Container()),
+                            ElevatedButton(
+                              onPressed: () => {},
+                              child: Column(
+                                // Replace with a Row for horizontal icon + text
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.door_sliding_sharp,
+                                    size: 50,
+                                  ),
+                                  Text("Дверь")
+                                ],
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0.0),
+                            ),
+                            Expanded(child: Container()),
+                            ElevatedButton(
+                              onPressed: () => {},
+                              child: Column(
+                                // Replace with a Row for horizontal icon + text
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.tv,
+                                    size: 50,
+                                  ),
+                                  Text("Проектор")
+                                ],
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.transparent,
+                                  elevation: 0.0),
+                            ),
+                            Expanded(child: Container()),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40))),
+                          width: 300,
+                          height: 500,
+                        )
+                      ],
+                    )
 
-                  Container(
-                    height: displayWidth * .285,
-                  )
-                ],
-              );
-            },
+                    /*Center(
+                    child: Text(selectedCab != null
+                        ? 'Selected Cab: ${selectedCab!}'
+                        : 'No cab selected'),
+                  ),
+                  */
+                    ),
+              ),
+            ],
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
